5ac7c826c12ca648cf0736b590626347d289f8de
Odin <andreas.kammerloher@arcor.de>
2017-07-10 21:46:06 +0200

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index');
+});
+
+router.get('/preise', function (req, res, next) {
+    res.render('preise');
+});
+
+router.get('/anfahrt', function (req, res, next) {
+    res.render('anfahrt');
+});
+
+router.get('/zeiten', function (req, res, next) {
+    res.render('zeiten');
+});
+
+/* GET ABOUT page. */
+router.get('/about', function (req, res, next) {
+    res.render('about');
+});
+
+/* GET IMPRESSUM page. */
+router.get('/impressum', function (req, res, next) {
+    res.render('impressum');
+});
+
+module.exports = router;
