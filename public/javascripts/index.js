cf0fcdac7fd963bd9d0c616cbb5d31099f5f5e02
Odin <andreas.kammerloher@arcor.de>
2017-07-11 13:36:40 +0200

    Highlighted selected navbar item

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,4 +4,21 @@
 
 var date = new Date();
 
-$("#day"+date.getDay()).css("color", "#1E81E0");
+$("#day" + date.getDay()).css("color", "#1E81E0");
+
+
+// Highlight navbar items
+var navItemColor = "white";
+
+if (window.location.href.indexOf("kontakt") > -1) {
+    $("#kontakt").css("color", navItemColor);
+}
+else if (window.location.href.indexOf("anfahrt") > -1) {
+    $("#anfahrt").css("color", navItemColor);
+}
+else if (window.location.href.indexOf("preise") > -1) {
+    $("#preise").css("color", navItemColor);
+}
+else {
+    $("#home").css("color", navItemColor);
+}
